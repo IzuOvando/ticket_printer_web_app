0d30b73028bd81002f2674dc09829427017f2d2a
Ruben35 <ruben111416@gmail.com>
2024-06-28 23:25:14 -0600

    ✨ Adding context menu for printers

--- src/components/printers/PrinterAsideCard.tsx
+++ src/components/printers/PrinterAsideCard.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Printer } from "lucide-react";
+import { Printer, Pencil, Trash2 } from "lucide-react";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu";
 
 const PrinterAsideCard = ({
   name,
@@ -11,29 +17,59 @@ const PrinterAsideCard = ({
   online: boolean;
 }) => {
   return (
-    <div className=" flex items-center flex-col rounded-md border p-4 w-full">
-      <Printer
-        size={90}
-        strokeWidth={6}
-        absoluteStrokeWidth
-        color="rgb(var(--accent-dark-color))"
-      />
-      <div className="w-full flex flex-col items-center">
-        <span className="font-bold text-xl text-center">{name}</span>
-        <span className="font-medium text-lg text-gray-600">{ip}</span>
-        {online ? (
-          <span className="flex gap-2 justify-center items-center text-green-500 font-medium">
-            <span className="flex h-2 w-2 rounded-full bg-green-500" />
-            Online
-          </span>
-        ) : (
-          <span className="flex gap-2 justify-center items-center text-red-500 font-medium">
-            <span className="flex h-2 w-2 rounded-full bg-red-500" />
-            Offline
-          </span>
-        )}
-      </div>
-    </div>
+    <ContextMenu>
+      <ContextMenuTrigger>
+        <div className=" flex items-center flex-col rounded-md border p-4 w-full select-none">
+          <Printer
+            size={90}
+            strokeWidth={6}
+            absoluteStrokeWidth
+            color="rgb(var(--accent-dark-color))"
+          />
+          <div className="w-full flex flex-col items-center">
+            <span className="font-bold text-xl text-center">{name}</span>
+            <span className="font-medium text-lg text-gray-600">{ip}</span>
+            {online ? (
+              <span className="flex gap-2 justify-center items-center text-green-500 font-medium">
+                <span className="flex h-2 w-2 rounded-full bg-green-500" />
+                Online
+              </span>
+            ) : (
+              <span className="flex gap-2 justify-center items-center text-red-500 font-medium">
+                <span className="flex h-2 w-2 rounded-full bg-red-500" />
+                Offline
+              </span>
+            )}
+          </div>
+        </div>
+      </ContextMenuTrigger>
+      <ContextMenuContent className="w-44">
+        <ContextMenuItem
+          className="text-md font-medium flex justify-between items-center cursor-pointer"
+          inset
+        >
+          Editar
+          <Pencil
+            size={18}
+            strokeWidth={1.5}
+            absoluteStrokeWidth
+            className="text-slate-950"
+          />
+        </ContextMenuItem>
+        <ContextMenuItem
+          className="text-md font-medium flex justify-between items-center cursor-pointer hover:!bg-red-100"
+          inset
+        >
+          Eliminar
+          <Trash2
+            size={18}
+            strokeWidth={1.5}
+            absoluteStrokeWidth
+            className="text-slate-950"
+          />
+        </ContextMenuItem>
+      </ContextMenuContent>
+    </ContextMenu>
   );
 };
 
