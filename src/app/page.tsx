a93d9f87739e11c6ed10fd7bb62cad836f8c86d5
Ruben35 <ruben111416@gmail.com>
2024-06-28 19:55:32 -0600

    ✨ Sign in finish and sign out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,4 @@
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-
 export default async function Home() {
-  const session = await getServerSession();
-
-  console.log(session);
-
-  if (!session) {
-    return redirect("/login");
-  }
-
   return (
     <main>
       <h1>Here implement the tables and main app</h1>
